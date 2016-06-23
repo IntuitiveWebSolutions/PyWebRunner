713a746fd7fc408e330aecd23e406178224e7b89
Scott <sblevins@gmail.com>
2016-06-23 10:53:49 -0500

    Tweaks

---
+++ PyWebRunner/__init__.py
@@ -0,0 +1,1 @@
+from WebRunner import WebRunner
--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ if os.path.exists('README.rst'):
 
 setup(
     name='PyWebRunner',
-    version='1.0.1',
+    version='1.0.2',
     url='http://github.com/IntuitiveWebSolutions/PyWebRunner',
     license='MIT',
     author='Scott Blevins',
