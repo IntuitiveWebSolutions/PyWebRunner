dde2f6f80d0a61200c55a76cc75078f22735219d
Scott <sblevins@gmail.com>
2016-06-30 22:05:50 -0500

    Fixing tests

--- tests/HttpBase.py
+++ tests/HttpBase.py
@@ -1,32 +1,61 @@
+import os
+import sys
 import unittest
-import threading
+import wait
+
+from subprocess import Popen
 from PyWebRunner import WebTester
-from six.moves import SimpleHTTPServer
-try:
-    import SocketServer
-except ImportError:
-    import socketserver as SocketServer
 
+class Server(object):
+    """
+    A server class for running a SimpleHTTPServer as a process.
+    """
 
-class HTTPTestServer(SocketServer.TCPServer):
-    allow_reuse_address = True
+    port = 8000
+    env = os.environ.copy()
+    root = os.path.join(os.path.dirname(__file__), os.pardir)
+    process = None
+    extra_verbose = False
+    silence = open(os.devnull, 'w')
 
+    def start(self):
+        if sys.version_info >= (3,0):
+            module = 'http.server'
+        else:
+            module = 'SimpleHTTPServer'
+        command = ['python', '-m', module]
+
+        if self.extra_verbose:
+            self.process = Popen(command, cwd=self.root, env=self.env)
+        else:
+            self.process = Popen(command, cwd=self.root, stdout=self.silence, stderr=self.silence, env=self.env)
+
+        wait.tcp.open(self.port)
+
+    def stop(self):
+        try:
+            self.process.kill()
+        except:
+            print("Could not kill server process.")
+            # Try to kill this process but don't fail completely if we can't
+            # for some reason.
+            pass
+
+    def restart(self):
+        self.stop()
+        self.start()
 
 class HttpBase(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-        cls.httpd = HTTPTestServer(("", 5000), Handler)
-        cls.httpd_thread = threading.Thread(target=cls.httpd.serve_forever)
-        cls.httpd_thread.setDaemon(True)
-        cls.httpd_thread.start()
+        cls.server = Server()
+        cls.server.start()
 
-        cls.wt = WebTester()
+        cls.wt = WebTester(base_url='http://127.0.0.1:8000/')
         cls.wt.start()
 
     @classmethod
     def tearDownClass(cls):
         cls.wt.stop()
-        cls.httpd.shutdown()
-        cls.httpd_thread.join()
+        cls.server.stop()
