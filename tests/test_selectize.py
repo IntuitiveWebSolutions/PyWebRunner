124a0bebe871cf971727f0255632192b73380272
Jacob <jacob@britecore.com>
2016-06-30 21:21:25 -0500

    Adding basic test for set_selectize

---
+++ tests/test_selectize.py
@@ -0,0 +1,12 @@
+from HttpBase import HttpBase
+
+
+class TestSelectize(HttpBase):
+
+    def test_set_selectize(self):
+        self.wt.goto('/tests/html/selectize.html')
+        select_el = '#testSelectize'
+        test_value = 'Option 2'
+        self.wt.wait_for_visible(select_el + ' + .selectize-control')
+        self.wt.set_selectize(select_el, test_value)
+        assert self.wt.get_value(select_el) == test_value
